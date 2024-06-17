4fcf340f204e0d5645578d6cc78200cf5a872e8e
Stephan Bröker <webmaster@hostmy.de>
2024-06-17 08:26:58 +0200

    Update Ownable.sol

--- access/Ownable.sol
+++ access/Ownable.sol
@@ -3,50 +3,85 @@ pragma solidity 0.8.19;
 
 import {Context} from "../utils/Context.sol";
 
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address and provides basic authorization control
+ * functions, simplifying the implementation of "user permissions".
+ */
 abstract contract Ownable is Context {
     address private _owner;
 
-    error OwnableUnauthorizedAccount(address account);
-    error OwnableInvalidOwner(address owner);
+    error OwnableUnauthorizedAccount(address account); // Error for unauthorized access
+    error OwnableInvalidOwner(address owner); // Error for invalid owner address
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner); // Event emitted when ownership is transferred
 
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     * @param initialOwner The address of the initial owner.
+     */
     constructor(address initialOwner) {
         if (initialOwner == address(0)) {
-            revert OwnableInvalidOwner(address(0));
+            revert OwnableInvalidOwner(address(0)); // Reverts if the initial owner is the zero address
         }
-        _transferOwnership(initialOwner);
+        _transferOwnership(initialOwner); // Sets the initial owner
     }
 
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
     modifier onlyOwner() {
-        _checkOwner();
+        _checkOwner(); // Checks if the caller is the owner
         _;
     }
 
+    /**
+     * @dev Returns the address of the current owner.
+     * @return The address of the owner.
+     */
     function owner() public view virtual returns (address) {
-        return _owner;
+        return _owner; // Returns the address of the current owner
     }
 
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
     function _checkOwner() internal view virtual {
         if (owner() != _msgSender()) {
-            revert OwnableUnauthorizedAccount(_msgSender());
+            revert OwnableUnauthorizedAccount(_msgSender()); // Reverts if the caller is not the owner
         }
     }
 
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
     function renounceOwnership() public virtual onlyOwner {
-        _transferOwnership(address(0));
+        _transferOwnership(address(0)); // Allows the owner to renounce ownership, setting owner to the zero address
     }
 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     * @param newOwner The address of the new owner.
+     */
     function transferOwnership(address newOwner) public virtual onlyOwner {
         if (newOwner == address(0)) {
-            revert OwnableInvalidOwner(address(0));
+            revert OwnableInvalidOwner(address(0)); // Reverts if the new owner is the zero address
         }
-        _transferOwnership(newOwner);
+        _transferOwnership(newOwner); // Transfers ownership to a new address
     }
 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     * @param newOwner The address of the new owner.
+     */
     function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
+        address oldOwner = _owner; // Stores the current owner address
+        _owner = newOwner; // Updates the owner address
+        emit OwnershipTransferred(oldOwner, newOwner); // Emits an event indicating ownership transfer
     }
 }
